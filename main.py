d88214598a825db5a5934850a553eb844bb2c6b2
Iuri Sampaio <iurisampaio18@gmail.com>
2020-07-02 03:52:43 -0300

    last commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from urna import *
 
-adm = administrador('123456','iuri')
+adm = administrador('123','root')
 nome = input('digite o nome do adiminstrador: ')
 senha = input("insira uma senha: ")
 if senha != adm.MASTER_PASSWORD and nome != adm.nomeAdm:
@@ -34,34 +34,62 @@ else:
                         cargo = input("digite oque essa pessoa vai fazer: ")
                         nome = input("digite o nome: ")
                         senha = input("digite a senha: ")
-                        sec.inserirPessoa(cargo,nome,senha)
-                        
+                        sec.inserirPessoa(cargo.lower(),nome,senha)
                     if op == '2':
                         numero = input("digite o numero do candidato")
                         nome = input("digite o nome do candidato")
                         funcao = input("digite a função para qual o candidato se elegeu")
                         sec.inserirCandiato(numero,nome,funcao)
                     if op == '3':
-                        cpf=input("digite seu cpf: ")
-                        nome=input("digite seu nome: ")
-                        print("============OPÇÕES DE VOTO===========")
-                        sec.mostrarCandidatos()
-                        voto=input("em quem vc vai votar? ")
-                        sec.inserirEleitor(cpf,nome,voto)
-                    if op == '4':
                         sec.mostrarMesariosPresidentes()
-                    if op == '5':
+                    if op == '4':
                         sec.mostrarCandidatos()
-                    if op == '6':
-                        sec.mostarEleitores()
-                    if op == '7':
-                        sec.computarVotos()
+                    if op == '5':
+                        nomeP = input('digite o nome do presidente: ')
+                        senhaP = input('digite a senha: ')
+                        if(sec.loginPresidente(nomeP,senhaP)):
+                            pres = presidente(nomeP,senhaP)
+                            while True:
+                                pres.menu()
+                                ops = input("oque você deseja fazer ?")
+                                if ops == '1':
+                                    pres.inicioVotacao()
+                                if ops == '2':
+                                    nomeM = input('digite p nome do mesario: ')
+                                    senhaM = input('digite a senha: ')
+                                    if(pres.loginMesario(nomeM,senhaM)):
+                                        mes = mesario(nomeM,senhaM)
+                                        while True:
+                                            mes.menu()
+                                            op = input("oque você deseja fazer?")
+                                            if op == '1':
+                                                cpf=input("digite seu cpf: ")
+                                                nome=input("digite seu nome: ")
+                                                print("============OPÇÕES DE VOTO===========")
+                                                sec.mostrarCandidatos()
+                                                voto=input("em quem vc vai votar? ")
+                                                mes.inserirEleitor(cpf,nome,voto)
+                                            if op =='2':
+                                                break
+                                    else:
+                                        print('mesario não cadastrado')
+                                if ops == '3':
+                                    pres.mostarEleitores()
+                                if ops == '4':
+                                    pres.computarVotos()
+                                    input("pressione enter para sair")
+                                    break
+                                else:
+                                    print("opção não cadastrada")
+                                    continue
+                        else:
+                            print('esse cara não ta cadatrado') 
+                            continue
                     if op == '99':
                         break                      
             else:
                 print("não existe esse secretario")
                 continue
-
         if op == '99':
             break
             
--- urna.py
+++ urna.py
@@ -11,8 +11,7 @@ class administrador:
         self.MASTER_PASSWORD = MASTER_PASSWORD
         self.nomeAdm = nomeAdm
 
-    def barra(self, n=40, caractere='*'):
-        print (caractere*n)
+    barra = lambda self, n=40, caractere='*':print(caractere*n) 
 
     def menu(self):
         self.barra()
@@ -58,16 +57,12 @@ class administrador:
         self.nome = nomeSecretario
         self.senha = senhaSecretario
         self.cursor.execute(f'''
-        SELECT * FROM tbl_secretario 
-        where nomeUsuario = '{self.nome}'
-        and password = '{self.senha}'
+        SELECT * FROM tbl_secretario
         ''')
-        if self.cursor.rowcount == 0 :
-            print('Esse usuario não está cadastrado !!')
-            return False
-        else:
-            print(f'Olá {self.nome}')
-            return True
+        for i in self.cursor.fetchall():
+            if (i[0]==self.nome and i[1]==self.senha) :
+                print(f'Olá {self.nome}')
+                return True
 
 class secretario:
     conex = sqlite3.connect('loginUsers.db')
@@ -104,11 +99,9 @@ class secretario:
         self.barra()
         print("$ 1 -> Cadastrar Presidente ou Mesario")
         print("$ 2 -> Cadastrar Candidato")
-        print("$ 3 -> Cadastrar Eleitor")
-        print("$ 4 -> Mostar mesarios e presidentes do TRE")
-        print("$ 5 -> Mostar Candidatos")
-        print("$ 6 -> Mostrar eleitores e votos")
-        print("$ 7 -> Computar votos")
+        print("$ 3 -> Mostar mesarios e presidentes do TRE")
+        print("$ 4 -> Mostar Candidatos")
+        print("$ 5 -> Entrar como Presidente")        
         print("$ 99 -> sair do sistema")
         self.barra()
 
@@ -121,7 +114,7 @@ class secretario:
             ('{self.service}','{self.nomeUsuario}','{self.password}') 
         ''')
         self.conex.commit()
-
+    
     def inserirCandiato(self, idCandidato, nomeCandidato,funcaoCandidatura ):
         self.idCandidato = idCandidato
         self.nomeCandidato = nomeCandidato
@@ -132,6 +125,17 @@ class secretario:
         ''')
         self.conex.commit()
 
+    def mostrarMesariosPresidentes(self):
+        self.cursor.execute('''
+            SELECT * FROM tbl_pessoa
+        ''')
+        for i in self.cursor.fetchall():
+            self.barra()
+            print (f'Serviço: {i[0]} \nNome: {i[1]} \nSenha: {i[2]} ')
+        input("press enter to continue...")
+        self.conex.commit()
+
+    
     def mostrarCandidatos(self):
         self.cursor.execute('''
             SELECT * FROM tbl_candidato2
@@ -141,24 +145,81 @@ class secretario:
             print (f'Código para voto: {i[0]} \nNome: {i[1]}\nFunção: {i[2]} ')
         input("press enter to continue...")
 
-    def inserirEleitor(self, cpf, nomeEleitor, votoEleitor):
-        self.cpf = cpf
-        self.nomeEleitor = nomeEleitor
-        self.votoEleitor = votoEleitor
-        self.cursor.execute(f'''
-            INSERT INTO tbl_eleitor VALUES
-            ('{self.cpf}','{self.nomeEleitor}','{self.votoEleitor}')
-            ''')
-        self.conex.commit()
-
-    def mostrarMesariosPresidentes(self):
+    def loginPresidente(self, nome,senha):
+        self.nomeP=nome
+        self.senhaP=senha
         self.cursor.execute('''
             SELECT * FROM tbl_pessoa
         ''')
         for i in self.cursor.fetchall():
-            self.barra()
-            print (f'Serviço: {i[0]} \nNome: {i[1]} \nSenha: {i[2]} ')
-        input("press enter to continue...")
+            if (i[0].lower()=='presidente' and i[1]==self.nomeP and i[2]==self.senhaP):
+                print(f'Ola {self.nomeP}')
+                return True
+        
+        self.conex.commit()
+
+class presidente:
+    conex = sqlite3.connect('loginUsers.db')
+    cursor = conex.cursor()
+    def __init__(self,nome,senha):
+        self.senhaP=senha
+        self.nomeP=nome
+
+    barra = lambda self, n=40, caractere='*':print(caractere*n) 
+
+    def menu(self):
+        self.barra()
+        print("$ 1 -> Iniciar Votação e limpar votos")
+        print("$ 2 -> Entrar como Mesario")
+        print("$ 3 -> Mostrar Eleitores")
+        print("$ 4 -> Fechar a votação e computar votos")
+        self.barra()
+    
+    def inicioVotacao(self):
+        self.cursor.execute('''
+        TRUNCATE TABLE tbl_eleitor
+        ''')
+        if self.cursor.rowcount==0:
+            print("não foi possivel limpar a votação")
+        else:
+            print("dados excluidos e votação iniciada")
+        self.conex.commit()
+
+    def loginMesario(self,nome,senha):
+        self.nomeM=nome
+        self.senhaM=senha
+        self.cursor.execute('''
+            SELECT * FROM tbl_pessoa
+        ''')
+        for i in self.cursor.fetchall():
+            if (i[0].lower()=='mesario' and i[1]==self.nomeM and i[2]==self.senhaM):
+                print(f'Ola {self.nomeM}')
+                return True
+        
+        self.conex.commit()
+
+    def computarVotos(self):
+        votos=[]
+        candidatos=[]
+        n=[]
+        self.cursor.execute('''
+        SELECT idVoto from tbl_eleitor
+        ''')
+        for i in self.cursor.fetchall():
+            votos.append(i[0])
+        
+        self.cursor.execute('''
+        SELECT idCandidato from tbl_candidato2
+        ''')
+        for i in self.cursor.fetchall():
+            candidatos.append(i[0])
+        for i in range(votos.__len__()):
+            n.append(list(filter(lambda x: x==candidatos[i] , votos)))
+            if n[i]==[]:
+                print(f'O candidato {candidatos[i]} não recebeu nenhum voto')
+            else:
+                print(f'o candidato {candidatos[i]} recebeu {n[i].__len__()} votos')
+        self.conex.commit()
 
     def mostarEleitores(self):
         self.cursor.execute('''
@@ -172,17 +233,29 @@ class secretario:
             for j in self.cursor.fetchall():
                 print (f'CPF: {i[0]} \nNome: {i[1]} \nVotou em: {j[1]} para {j[2]} ')
         input("press enter to continue...")
+        self.conex.commit() 
 
-    def computarVotos(self):
-        self.cursor.execute('''
-        SELECT idVoto from tbl_eleitor
-        ''')
-        for i in self.cursor.fetchall():
-            self.cursor.execute(f'''
-            SELECT * from tbl_candidato2          
+class mesario:
+    conex = sqlite3.connect('loginUsers.db')
+    cursor = conex.cursor()
+    def __init__(self,nome,senha):
+        self.nome=nome
+        self.senha=senha
+    
+    barra = lambda self, n=40, caractere='*':print(caractere*n) 
+    
+    def menu(self):
+        self.barra()
+        print("$ 1 -> Liberar votação para eleitores")
+        print("$ 2 -> Voltar para nivel Presidente")
+        self.barra()
+    
+    def inserirEleitor(self, cpf, nomeEleitor, votoEleitor):
+        self.cpf = cpf
+        self.nomeEleitor = nomeEleitor
+        self.votoEleitor = votoEleitor
+        self.cursor.execute(f'''
+            INSERT INTO tbl_eleitor VALUES
+            ('{self.cpf}','{self.nomeEleitor}','{self.votoEleitor}')
             ''')
-            for j in self.cursor.fetchall():
-                #print(j[0], i[0])
-                if(j[0] == i[0]):
-                    print(i[0])
-        
+        self.conex.commit()
